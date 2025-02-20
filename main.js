de2da4106bac498d168d63c44cdec21b3f0f5860
Shadowpmr <upmromero@gmail.com>
2025-02-19 22:50:27 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -22,11 +22,11 @@ function gotResults(error, results){
         document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
         document.getElementById("result_confidence").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
     
-        img = document.getElementById('alarm1') 
-        img1 = document.getElementById('alarm2')
-        img2 = document.getElementById('alarm3')
-        img3 = document.getElementById('alarm4')
-        img4 = document.getElementById('alarm5')
+        img = document.getElementById("alarm1") 
+        img1 = document.getElementById("alarm2")
+        img2 = document.getElementById("alarm3")
+        img3 = document.getElementById("alarm4")
+        img4 = document.getElementById("alarm5")
     
         if (results[0].label == "Ruido de fondo") {
           img.src = 'Fuego.png';
@@ -34,36 +34,42 @@ function gotResults(error, results){
           img2.src = 'ambulancia.png';
           img3.src = 'tsunami.webp';
           img4.src = 'sismo.webp';
-        } else if (results[0].label == "Fuego") {
+        } else if (results[0].label == "alarma incendios") {
           img.src = 'Fuego.gif';
           img1.src = 'vidrio.png';
           img2.src = 'ambulancia.png';
           img3.src = 'tsunami.webp';
           img4.src = 'sismo.webp';
-        } else if (results[0].label == "Vidrio") {
+        } else if (results[0].label == "alarma vidrios rotos") {
           img.src = 'Fuego.png';
           img1.src = 'vidrio.gif';
           img2.src = 'ambulancia.png';
           img3.src = 'tsunami.webp';
           img4.src = 'sismo.webp';
-        } else if (results[0].label == "Ambulancia") {
+        } else if (results[0].label == "alarma ambulancia ") {
           img.src = 'Fuego.png';
           img1.src = 'vidrio.png';
           img2.src = 'ambulancia.gif';
           img3.src = 'tsunami.webp';
           img4.src = 'sismo.webp';
-        } else if (results[0].label == "Tsunami") { 
+        } else if (results[0].label == "alarma tsunami") { 
           img.src = 'Fuego.png';
           img1.src = 'vidrio.png';
           img2.src = 'ambulancia.png';
           img3.src = 'tsunami.gif';
           img4.src = 'sismo.webp';
-        } else{
+        } else if (results[0].label == "alarma sismo") { 
+            img.src = 'Fuego.png';
+            img1.src = 'vidrio.png';
+            img2.src = 'ambulancia.png';
+            img3.src = 'tsunami.webp';
+            img4.src = 'sismo.gif';
+        } else {
           img.src = 'Fuego.png';
           img1.src = 'vidrio.png';
           img2.src = 'ambulancia.png';
           img3.src = 'tsunami.webp';
-          img4.src = 'sismo.gif';
+          img4.src = 'sismo.webp';
         }
       }    
     }
